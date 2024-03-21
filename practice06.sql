63c5c18b6fe2e2af5727ca4047ee549d3d6f0e8b
leofuoq <112978597+leofuoq@users.noreply.github.com>
2024-03-21 08:33:46 +0700

    Update practice06.sql

--- practice06.sql
+++ practice06.sql
@@ -54,10 +54,26 @@ WHERE b.month = 7
 GROUP BY b.month
   
 --Bài 6-- 
-
+SELECT 
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(id) AS trans_count,
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(amount) as trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country
 
 --Bài 7--
-  
+WITH firstY AS(
+SELECT product_id , min(year) AS first_year 
+FROM Sales 
+GROUP BY product_id)
+SELECT  a.product_id, b.first_year, a.quantity, a.price FROM Sales as a
+JOIN firstY as b
+USING(product_id)
+WHERE a.year = b.first_year
+
 --Bài 8--
 SELECT customer_id FROM customer 
 GROUP BY customer_id
@@ -77,8 +93,21 @@ GROUP BY company_id,title, description)
 SELECT count(*) FROM list2
 WHERE count >1
 
---Bài 11-- need help
-
+--Bài 11-- 
+WITH listCus AS (
+SELECT distinct user_id, count(movie_id),user.name as name from MovieRating JOIN Users as user USING(user_id)
+GROUP BY user_id 
+ORDER BY count(movie_id) DESC, user.name
+limit 1),
+list_id AS (SELECT distinct movie_id, avg(rating), movie.title as title FROM MovieRating  JOIN Movies as movie using(movie_id) 
+WHERE DATE_FORMAT(created_at, '%Y-%m') = '2020-02'
+GROUP BY movie_id
+ORDER BY AVG(rating) DESC, movie.title  
+limit 1)
+(SELECT name as results from listCus)
+UNION ALL
+(SELECT title  FROM list_id)
+  
 --Bài 12--
 WITH list AS ((SELECT requester_id AS id FROM RequestAccepted)
 UNION ALL (SELECT accepter_id FROM RequestAccepted))
