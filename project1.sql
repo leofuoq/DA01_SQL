87fe79b070351b65472f8863df3f9b8294a38ce2
leofuoq <112978597+leofuoq@users.noreply.github.com>
2024-03-25 14:24:54 +0700

    Update project1.sql

--- project1.sql
+++ project1.sql
@@ -16,3 +16,54 @@ SELECT * FROM sales_dataset_rfm_prj WHERE SALES IS NULL OR LENGTH(NULLIF(LTRIM(R
 SELECT * FROM sales_dataset_rfm_prj WHERE ORDERDATE IS NULL OR LENGTH(NULLIF(LTRIM(RTRIM(ORDERDATE)), '')) = 0;
 
 --Bài 3--
+--Thêm cột CONTACTLASTNAME, CONTACTFIRSTNAME được tách ra từ CONTACTFULLNAME.--
+Alter table sales_dataset_rfm_prj add CONTACTLASTNAME varchar
+Alter table sales_dataset_rfm_prj add CONTACTFIRSTNAME varchar
+
+UPDATE sales_dataset_rfm_prj
+SET CONTACTLASTNAME = SUBSTRING(
+    CONTACTFULLNAME, 
+    POSITION('-' IN CONTACTFULLNAME) + 1, 
+    LENGTH(CONTACTFULLNAME) - POSITION('-' IN CONTACTFULLNAME)
+)
+UPDATE sales_dataset_rfm_prj
+SET CONTACTFIRSTNAME = SUBSTRING(
+    CONTACTFULLNAME,1, 
+    POSITION('-' IN CONTACTFULLNAME)-1)
+
+--Chuẩn hóa theo định dạng chữ cái đầu tiên viết hoa, chữ cái tiếp theo viết thường--
+UPDATE sales_dataset_rfm_prj
+SET CONTACTFIRSTNAME = CONCAT(
+    UPPER(SUBSTRING(CONTACTFIRSTNAME, 1, 1)), 
+    LOWER(SUBSTRING(CONTACTFIRSTNAME, 2))
+);
+
+UPDATE sales_dataset_rfm_prj
+SET CONTACTLASTNAME = CONCAT(
+    UPPER(SUBSTRING(CONTACTLASTNAME, 1, 1)), 
+    LOWER(SUBSTRING(CONTACTLASTNAME, 2))
+);
+
+--Bài 4--Thêm cột QTR_ID, MONTH_ID, YEAR_ID lần lượt là Qúy, tháng, năm được lấy ra từ ORDERDATE 
+--Tạo bảng--
+Alter table sales_dataset_rfm_prj add QTR_ID varchar;
+Alter table sales_dataset_rfm_prj add MONTH_ID varchar;
+Alter table sales_dataset_rfm_prj add YEAR_ID varchar;
+--Add dữ liệu--
+UPDATE sales_dataset_rfm_prj
+SET QTR_ID =
+CASE
+	WHEN EXTRACT(MONTH FROM orderdate) IN (1,2,3) THEN 1
+	WHEN EXTRACT(MONTH FROM orderdate) IN (4,5,6) THEN 2
+	WHEN EXTRACT(MONTH FROM orderdate) IN (7,8,9) THEN 3
+	WHEN EXTRACT(MONTH FROM orderdate) IN (10,11,12) THEN 4
+END 
+
+UPDATE sales_dataset_rfm_prj
+SET MONTH_ID = EXTRACT(MONTH FROM orderdate) 
+
+UPDATE sales_dataset_rfm_prj
+SET YEAR_ID = EXTRACT(YEAR FROM orderdate)
+
+--Bài 5-- Hãy tìm outlier (nếu có) cho cột QUANTITYORDERED và hãy chọn cách xử lý cho bản ghi đó (2 cách) ( Không chạy câu lệnh trước khi bài được review)
+
