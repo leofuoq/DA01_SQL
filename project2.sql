9770757337cbc81d8474d6dd367280f51c32f5ec
leofuoq <112978597+leofuoq@users.noreply.github.com>
2024-12-31 19:07:25 +0700

    Update project2.sql

--- project2.sql
+++ project2.sql
@@ -1,114 +1,143 @@
---Bài 1-- 
-WITH A AS(SELECT FORMAT_DATE('%Y-%m',created_at) as year_month,
-user_id,order_id, status 
-from bigquery-public-data.thelook_ecommerce.orders
-order by year_month)
-SELECT year_month, count(user_id) as total_user, 
-SUM(CASE WHEN status='Complete' THEN 1 else 0 END) as total_order_completed
-FROM A
-GROUP BY year_month
-ORDER BY year_month
+--GOOGLE CLOUD--
+--II. Ad-hoc tasks
+--1-- Thống kê tổng số lượng người mua và số lượng đơn hàng đã hoàn thành mỗi tháng ( Từ 1/2019-4/2022)
 
---Bài 2--
-WITH A AS (SELECT FORMAT_DATE('%Y-%m',created_at) as year_month,
-user_id,order_id, sale_price
-FROM bigquery-public-data.thelook_ecommerce.order_items
-WHERE created_at between '2019-01-01' and '2022-04-30' )
+-- (*) Note: Vì yêu cầu số lượng đơn hàng đã hoàn thành (aka status 'Complete') --> dùng 'delivered_date' ở bảng oder_items làm mốc thời gian ghi nhận 
+Select 
+FORMAT_DATE('%Y-%m', t2.delivered_at) as month_year, 
+count(DISTINCT t1.user_id) as total_user,
+count(t1.ORDER_id) as total_order
+from bigquery-public-data.thelook_ecommerce.orders as t1
+Join bigquery-public-data.thelook_ecommerce.order_items as t2 
+on t1.order_id=t2.order_id
+Where t1.status='Complete' and 
+t2.delivered_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00'
+Group by month_year
+ORDER BY month_year
 
-SELECT year_month, count(DISTINCT user_id) as distinct_users,
-sum(sale_price)/count(order_id) AS average_order_value
-FROM A
-GROUP BY year_month
-ORDER BY year_month
---Bài 3--
-WITH A AS(select distinct first_name, last_name, gender, age,
-DENSE_RANK() OVER(PARTITION BY gender ORDER BY age) AS rank,
-FROM bigquery-public-data.thelook_ecommerce.order_items as a
-JOIN bigquery-public-data.thelook_ecommerce.users as b
-ON a.user_id = b.id
-WHERE a.created_at between '2019-01-01' and '2022-04-30'),
-B AS(select distinct first_name, last_name, gender, age,
-DENSE_RANK() OVER(PARTITION BY gender ORDER BY age DESC) AS rank,
-FROM bigquery-public-data.thelook_ecommerce.order_items as a
-JOIN bigquery-public-data.thelook_ecommerce.users as b
-ON a.user_id = b.id
-WHERE a.created_at between '2019-01-01' and '2022-04-30'),
-youngest AS(
-SELECT distinct first_name, last_name, gender, age,
-CASE WHEN rank = 1 Then 'youngest' END tag
-FROM A),
-oldest AS(
-SELECT distinct first_name, last_name, gender, age,
-CASE WHEN rank = 1 Then 'oldest' END tag
-FROM B) 
+/*--> Insight: 
+    - Nhìn chung số lượng người mua hàng và đơn hàng tiêu thụ đã hoàn thành tăng dần theo mỗi tháng và năm   
+    - Giai đoạn 2019-tháng 1 2022: người mua hàng có xu hướng mua sắm nhiều hơn vào ba tháng cuối năm (10-12) và tháng 1 năm kế tiếp do nhu cầu mua sắm cuối/đầu năm tăng 
+           và nhiều chương trình khuyến mãi/giảm giá cuối năm           
+    - Giai đoạn bốn tháng đầu năm 2022: ghi nhận tỷ lệ lượng người mua tăng mạnh so với ba tháng cuối năm 2021, khả năng do TheLook triển khai chương trình khuyến mãi mới nhằm 
+      kích cầu mua sắm các tháng đầu năm
+    - Tháng 7 2021 ghi nhận lượng mua hàng tăng bất thường, trái ngược với lượng mua giảm sút so với cùng kì năm 2020, có thể do TheLook triển khai campaign đặc biệt cải thiện tình hình 
+      doanh số cho riêng tháng 7.
+*/
 
-(SELECT first_name, last_name, gender, age,tag FROM youngest WHERE tag ='youngest')
+--2-- Giá trị đơn hàng trung bình (AOV) và số lượng khách hàng mỗi tháng
+--(*) Note: vì yêu cầu tập trung vào người dùng và không yêu cầu đơn hàng đã hoàn thành nên sử dụng 'created_at' (theo mình hiểu thì là ngày đơn hàng được tạo) làm mốc thời gian 
+Select 
+FORMAT_DATE('%Y-%m', created_at) as month_year,
+count(DISTINCT user_id) as distinct_users,
+round(sum(sale_price)/count(distinct order_id),2) as average_order_value
+from bigquery-public-data.thelook_ecommerce.order_items
+Where created_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00'
+Group by month_year
+ORDER BY month_year
+
+/*--> Insight: - Giai đoạn năm 2019 do số lượng người dùng ít khiến giá trị đơn hàng trung bình qua các tháng có tỷ lệ biến động cao.
+               - Giai đoạn từ cuối năm 2019 lượng người dùng ổn định trên 400 và nhìn chung tiếp tục tăng qua các tháng, giá trị đơn hàng trung bình qua các tháng ổn định ở mức ~80-90
+ */
+    
+/*    
+--3-- Nhóm khách hàng theo độ tuổi
+Tìm các khách hàng có trẻ tuổi nhất và lớn tuổi nhất theo từng giới tính ( Từ 1/2019-4/2022)
+*/
+
+With female_age as 
+(
+select min(age) as min_age, max(age) as max_age
+from bigquery-public-data.thelook_ecommerce.users
+Where gender='F' and created_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00'
+),
+male_age as 
+(
+select min(age) as min_age, max(age) as max_age
+from bigquery-public-data.thelook_ecommerce.users
+Where gender='M' and created_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00'
+),
+young_old_group as 
+(
+Select t1.first_name, t1.last_name, t1.gender, t1.age
+from bigquery-public-data.thelook_ecommerce.users as t1
+Join female_age as t2 on t1.age=t2.min_age or t1.age=t2.max_age
+Where t1.gender='F'and created_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00'
 UNION ALL
-(SELECT first_name, last_name, gender, age,tag FROM oldest WHERE tag ='oldest')
-ORDER BY tag
+Select t3.first_name, t3.last_name, t3.gender, t3.age
+from bigquery-public-data.thelook_ecommerce.users as t3
+Join female_age as t4 on t3.age=t4.min_age or t3.age=t4.max_age
+Where t3.gender='M' and created_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00'
+),
+age_tag as
+(
+Select *, 
+Case 
+     when age in (select min(age) as min_age
+     from bigquery-public-data.thelook_ecommerce.users
+     Where gender='F' and created_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00') then 'Youngest'
+     when age in (select min(age) as min_age
+     from bigquery-public-data.thelook_ecommerce.users
+     Where gender='M'and created_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00') then 'Youngest'
+     Else 'Oldest'
+END as tag
+from young_old_group 
+)
+Select gender, tag, count(*) as user_count
+from age_tag
+group by gender, tag
+  /*  
+  --> Insight: trong giai đoạn Từ 1/2019-4/2022
+      - Giới tính Female: lớn tuổi nhất là 70 tuổi (525 người người dùng); nhỏ tuổi nhất là 12 tuổi (569 người dùng)
+      - Giới tính Male: lớn tuổi nhất là 70 tuổi (529 người người dùng); nhỏ tuổi nhất là 12 tuổi (546 người dùng)
 
---Bài 4--
-WITH A AS(SELECT FORMAT_DATE('%Y-%m',created_at) as year_month,
-a.id as product_id, a.name as product_name, ROUND(b.sale_price,2) as sales, ROUND(a.cost,2), ROUND(b.sale_price - a.cost,2) as profit1
-FROM bigquery-public-data.thelook_ecommerce.products a
-JOIN bigquery-public-data.thelook_ecommerce.order_items b
-ON a.id = b.product_id
-WHERE b.status = 'Complete'),
-B AS(
-SELECT year_month, product_id, sum(profit1) as profit
-FROM A
-GROUP BY year_month, product_id),
-C AS(SELECT kaka.year_month, kaka.product_id, kaka.product_name, kaka.sales, haha.profit,
-DENSE_RANK() OVER(PARTITION BY kaka.year_month ORDER BY profit DESC) as rank_per_month 
-FROM A as kaka
-JOIN B as haha
-ON kaka.year_month = haha.year_month and kaka.product_id = haha.product_id)
-SELECT * FROM C
-WHERE rank_per_month <=5
-order by year_month
---Bài 5--
-WITH A AS(SELECT
-CASE
-  WHEN b.created_at between '2022-01-15' and '2022-02-15' THEN '15/1-15/2'
-  WHEN b.created_at between '2022-02-15' and '2022-03-15' THEN '15/2-15/3'
-  WHEN b.created_at between '2022-03-15' and '2022-04-15' THEN '15/3-15/4'
-END year_month,
-a.category as category, ROUND(b.sale_price,2) as sale
-FROM bigquery-public-data.thelook_ecommerce.products a
-JOIN bigquery-public-data.thelook_ecommerce.order_items b
-ON a.id = b.product_id
-WHERE b.status = 'Complete'
-AND b.created_at between '2022-01-15' and '2022-04-15'),
-B AS(
-SELECT year_month, category, ROUND(sum(sale),2) as revenue
-FROM A
-GROUP BY year_month, category)
-SELECT * FROM B
-ORDER BY year_month
+/*
+--4-- Thống kê top 5 sản phẩm có lợi nhuận cao nhất từng tháng (xếp hạng cho từng sản phẩm). 
+--Output: month_year ( yyyy-mm), product_id, product_name, sales, cost, profit, rank_per_month
+--Hint: Sử dụng hàm dense_rank()
+*/
+--(*) Note: đơn hàng tạo ra doanh thu là đơn hàng có status 'Complete' --> sử dụng deliver_date
+*/
 
-----
-WITH A AS(SELECT FORMAT_DATE('%Y-%m',created_at) as year_month,
-order_item.order_id ,
-product.category as product_category,
-order_item.sale_price,
-product.cost
-FROM bigquery-public-data.thelook_ecommerce.order_items order_item
-JOIN bigquery-public-data.thelook_ecommerce.products product
-ON order_item.product_id = product.id
-WHERE order_item.status = 'Complete'),
-B AS(
-SELECT year_month, product_category, ROUND(sum(sale_price),2) as TPV, count(order_id) as TPO, 
-ROUND(sum(cost),2) as total_cost
-FROM A
-GROUP BY year_month, product_category
-ORDER BY year_month, product_category)
-CREATE VIEW vw_ecommerce_analyst AS (
-SELECT year_month, product_category, TPV,TPO,
-ROUND((TPV - LAG(TPV) OVER(PARTITION BY product_category ORDER BY year_month))*100/LAG(TPV) OVER(PARTITION BY product_category ORDER BY year_month),2) ||' '||'%'  AS Revenue_growth,
-ROUND(( TPO - LAG(TPO) OVER(PARTITION BY product_category ORDER BY year_month))*100/LAG(TPO) OVER(PARTITION BY product_category ORDER BY year_month),2) ||' '||'%'  AS Order_growth,
-total_cost, ROUND(TPV - total_cost,2) AS total_profit, 
-ROUND((TPV - total_cost)/total_cost,2) AS Profit_to_cost_ratio
-FROM B) -- lệnh create view k chạy được phải tự làm bằng tay--
+Select * from 
+(With product_profit as
+(
+Select 
+CAST(FORMAT_DATE('%Y-%m', t1.delivered_at) AS STRING) as month_year,
+t1.product_id as product_id,
+t2.name as product_name,
+round(sum(t1.sale_price),2) as sales,
+round(sum(t2.cost),2) as cost,
+round(sum(t1.sale_price)-sum(t2.cost),2)  as profit
+from bigquery-public-data.thelook_ecommerce.order_items as t1
+Join bigquery-public-data.thelook_ecommerce.products as t2 on t1.product_id=t2.id
+Where t1.status='Complete'
+Group by month_year, t1.product_id, t2.name
+)
+Select * ,
+dense_rank() OVER ( PARTITION BY month_year ORDER BY month_year,profit) as rank
+from product_profit
+) as rank_table
+Where rank_table.rank<=5
+order by rank_table.month_year
+
+/*
+5.Doanh thu tính đến thời điểm hiện tại trên mỗi danh mục
+Thống kê tổng doanh thu theo ngày của từng danh mục sản phẩm (category) trong 3 tháng qua ( giả sử ngày hiện tại là 15/4/2022)
+Output: dates (yyyy-mm-dd), product_categories, revenue
+*/
+--(*) Note: đơn hàng tạo ra doanh thu là đơn hàng có status 'Complete' --> sử dụng deliver_date
+*/
+Select 
+CAST(FORMAT_DATE('%Y-%m-%d', t1.delivered_at) AS STRING) as dates,
+t2.category as product_categories,
+round(sum(t1.sale_price),2) as revenue,
+from bigquery-public-data.thelook_ecommerce.order_items as t1
+Join bigquery-public-data.thelook_ecommerce.products as t2 on t1.product_id=t2.id
+Where t1.status='Complete' and t1.delivered_at BETWEEN '2022-01-15 00:00:00' AND '2022-04-16 00:00:00'
+Group by dates, product_categories
+Order by dates
+
 /* III
 1) Sử dụng câu lệnh SQL để tạo ra 1 dataset như mong muốn và lưu dataset đó vào VIEW đặt tên là vw_ecommerce_analyst
 */
@@ -197,3 +226,13 @@ Select cohort_month,
 (100.00 - round(100.00* m3/m1,2)) || '%' as m3,
 (100.00 - round(100.00* m4/m1,2))|| '%' as m4
 from customer_cohort
+
+--> Chart Cohort https://docs.google.com/spreadsheets/d/1Ke2bhPAaG2rDjMfZdcWeTw-mevymA4JHXdOOZ6KVA1Q/edit#gid=505337796
+/*
+Nhìn chung hằng tháng TheLook ghi nhận số lượng người dùng mới tăng dần đều, thể hiện chiến dịch quảng cáo tiếp cận người dùng
+mới có hiệu quả.
+Tuy nhiên trong giai đoạn 4 tháng đầu tính từ lần mua hàng/sử dụng trang thương mại điện tử TheLook, tỷ lệ người dùng cũ
+quay lại sử dụng trong tháng kế tiếp khá thấp: dao động dưới 10% trong giai đoạn từ 2019-01 đến 2023-07 và tăng lên mức 
+trên 10% trong những tháng còn lại của năm 2023, trong đó cao nhất là tháng đầu tiên sau 2023-10 với 18.28%.
+ --> Tỷ lệ khách hàng trung thành thấp, TheLook nên xem xét cách quảng bá để thiếp lập và tiếp cận nhóm khách hàng trung thành
+nhằm tăng doanh thu từ nhóm này và tiết kiệm các chi phí marketing
